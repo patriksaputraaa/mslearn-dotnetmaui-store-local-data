c6449aa941fd5a749cb53d5114890b8ce2f20f1c
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2024-11-22 17:18:57 +0700

    Day 1

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,4 +1,6 @@
 ﻿namespace People;
+using SQLite;
+using People.Models;
 
 public class PersonRepository
 {
@@ -7,10 +9,16 @@ public class PersonRepository
     public string StatusMessage { get; set; }
 
     // TODO: Add variable for the SQLite connection
+    private SQLiteConnection conn;
 
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
+        // TODO: Add code to initialize the repository
+        if (conn != null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>();
     }
 
     public PersonRepository(string dbPath)
@@ -24,13 +32,13 @@ public class PersonRepository
         try
         {
             // TODO: Call Init()
-
+            Init();
             // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
             // TODO: Insert the new person into the database
-            result = 0;
+            result = conn.Insert(new Person { Name = name });
 
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
         }
@@ -46,7 +54,8 @@ public class PersonRepository
         // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
